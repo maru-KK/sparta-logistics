28ae1e3b98a81332a19b53f265b9490bf7a35e24
hyunsb <junghss0113@gmail.com>
2024-12-11 19:48:16 +0900

    Chore: Order service 베이스 코드 작성

---
+++ com.sparta.logistics.order/src/main/java/com/sparta/logistics/order/presentation/rest/exception/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sparta.logistics.product.presentation.rest.exception.exceptions;
+
+public class DataNotFoundException extends RuntimeException {
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
