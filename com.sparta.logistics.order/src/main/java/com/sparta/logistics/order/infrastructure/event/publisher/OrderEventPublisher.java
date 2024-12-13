9c2069066524e026b9589a8c4a7830cd1c7a24a3
hyunsb <junghss0113@gmail.com>
2024-12-13 12:57:33 +0900

    Feat: Kafka 이벤트 발행 기능 구현

---
+++ com.sparta.logistics.order/src/main/java/com/sparta/logistics/order/infrastructure/event/publisher/OrderEventPublisher.java
@@ -0,0 +1,31 @@
+package com.sparta.logistics.order.infrastructure.event.publisher;
+
+import com.sparta.logistics.order.domain.event.DomainEvent;
+import com.sparta.logistics.order.domain.event.DomainEventEnvelop;
+import com.sparta.logistics.order.domain.event.OrderCreateEvent;
+import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class OrderEventPublisher {
+
+    private final KafkaTemplate<String, DomainEventEnvelop<? extends DomainEvent>> kafkaTemplate;
+
+    @Value("${spring.kafka.producer.topic.order.create}")
+    private String orderCreateTopic;
+
+    public void publish(DomainEventEnvelop<OrderCreateEvent> eventEnvelop) {
+
+        CompletableFuture<SendResult<String, DomainEventEnvelop<? extends DomainEvent>>> send =
+            kafkaTemplate.send(orderCreateTopic, eventEnvelop);
+
+        log.info("OrderEventPublisher OrderCreate Event publish ={}", send);
+    }
+}
