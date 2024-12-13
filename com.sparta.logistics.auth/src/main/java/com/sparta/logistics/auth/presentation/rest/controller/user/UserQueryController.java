fcfdd9312a677de0eb9ad77460325d668f076a46
정현수 <junghss0113@gmail.com>
2024-12-13 20:34:10 +0900

    [#32] Auth Service - URI 변경 (#37)
    * Modify: 로그인, 회원가입 엔드포인트 uri 변경
    
    * Modify: 유저 단건 조회 엔드포인트 uri 변경

--- com.sparta.logistics.auth/src/main/java/com/sparta/logistics/auth/presentation/rest/controller/user/UserQueryController.java
+++ com.sparta.logistics.auth/src/main/java/com/sparta/logistics/auth/presentation/rest/controller/user/UserQueryController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/auth/user")
+@RequestMapping("/api/v1/auth/user")
 @RestController
 public class UserQueryController {
 
