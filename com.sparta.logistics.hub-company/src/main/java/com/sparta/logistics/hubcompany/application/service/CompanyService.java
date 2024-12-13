dee79fb592d785cfe99b8c955f630b0cbd2efea8
MinJu Kim <110008586+kgrace2366@users.noreply.github.com>
2024-12-13 17:13:17 +0900

    [#33] Hub-Company Service - 유저 별 허브, 업체 정보 조회 기능 구현 완료 (#34)
    * Add .gitignore to exclude unnecessary files
    
    * Feat: 유저 별 허브, 업체 정보 조회 기능 구현

--- com.sparta.logistics.hub-company/src/main/java/com/sparta/logistics/hubcompany/application/service/CompanyService.java
+++ com.sparta.logistics.hub-company/src/main/java/com/sparta/logistics/hubcompany/application/service/CompanyService.java
@@ -2,12 +2,16 @@ package com.sparta.logistics.hubcompany.application.service;
 
 import com.sparta.logistics.hubcompany.application.dto.CompanyRequestDto;
 import com.sparta.logistics.hubcompany.application.dto.CompanyResponseDto;
+import com.sparta.logistics.hubcompany.application.dto.HubCompanyResponseDto;
 import com.sparta.logistics.hubcompany.application.dto.HubResponseDto;
 import com.sparta.logistics.hubcompany.infrastructure.persistence.entity.CompanyEntity;
+
+import java.util.List;
 
 public interface CompanyService {
     CompanyResponseDto createCompany(CompanyRequestDto request, Long userId);
     CompanyEntity getCompanyById(Long companyId);
     HubResponseDto getHubByCompanyId(Long companyId);
     void updateCompany(Long companyId, CompanyRequestDto request, Long userId);
+    List<HubCompanyResponseDto> getHubsAndCompaniesByUserId(Long userId);
 }
