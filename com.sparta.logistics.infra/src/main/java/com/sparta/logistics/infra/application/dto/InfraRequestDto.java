ce31cca63a43ec5edd1ec358a77d7ebbf1b88078
MinJu Kim <110008586+kgrace2366@users.noreply.github.com>
2024-12-16 18:47:34 +0900

    [#30] AI 응답 기능 구현 (#62)
    * Add .gitignore to exclude unnecessary files
    
    * Feat: AI 응답 기능 구현

---
+++ com.sparta.logistics.infra/src/main/java/com/sparta/logistics/infra/application/dto/InfraRequestDto.java
@@ -0,0 +1,19 @@
+package com.sparta.logistics.infra.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InfraRequestDto {
+    private Long orderId;
+    private String userName;
+    private String productName;
+    private int productQuantity;
+    private String request;
+    private String originHub;
+    private String destinationHub;
+    private String companyAddress;
+    private String deliveryPersonName;
+    private String snsAccount;
+}
