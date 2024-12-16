94d15d74846d7e47da1dde4ed4542297e2097d50
kgrace2366 <kgrace2366@naver.com>
2024-12-16 21:49:00 +0900

    Feat: Hub 캐시 저장 및 조회 기능 구현

--- com.sparta.logistics.hub-company/src/main/java/com/sparta/logistics/hubcompany/application/service/HubService.java
+++ com.sparta.logistics.hub-company/src/main/java/com/sparta/logistics/hubcompany/application/service/HubService.java
@@ -2,6 +2,7 @@ package com.sparta.logistics.hubcompany.application.service;
 
 import com.sparta.logistics.hubcompany.application.dto.HubCreationRequestDto;
 import com.sparta.logistics.hubcompany.application.dto.HubResponseDto;
+import com.sparta.logistics.hubcompany.domain.Hub;
 import com.sparta.logistics.hubcompany.infrastructure.persistence.entity.HubEntity;
 
 import java.util.List;
--- com.sparta.logistics.hub-company/src/main/java/com/sparta/logistics/hubcompany/domain/Hub.java
+++ com.sparta.logistics.hub-company/src/main/java/com/sparta/logistics/hubcompany/domain/Hub.java
@@ -4,13 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @Getter
 @AllArgsConstructor
 @Builder
 public class Hub {
+    private Long hubId;
     private String name;
     private String address;
-    private Double latitude;
-    private Double longitude;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
     private Long userId;
+
+    public Long getHubId() {return hubId;}
+    public String getName() {return name;}
+    public String getAddress() {return address;}
+    public BigDecimal getLatitude() {return latitude;}
+    public BigDecimal getLongitude() {return longitude;}
+    public Long getUserId() {return userId;}
 }
