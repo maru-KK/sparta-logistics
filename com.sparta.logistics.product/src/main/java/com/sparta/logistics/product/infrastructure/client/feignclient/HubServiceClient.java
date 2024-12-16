b73fc9324efc0cbd0290530ca89e66357da85a41
hyunsb <junghss0113@gmail.com>
2024-12-16 21:19:16 +0900

    Modify: Modify HubCompany Service FeignClient uri, response Type

--- com.sparta.logistics.product/src/main/java/com/sparta/logistics/product/infrastructure/client/feignclient/HubServiceClient.java
+++ com.sparta.logistics.product/src/main/java/com/sparta/logistics/product/infrastructure/client/feignclient/HubServiceClient.java
@@ -1,15 +1,15 @@
 package com.sparta.logistics.product.infrastructure.client.feignclient;
 
 import com.sparta.logistics.product.infrastructure.client.dto.CompanyDetailClientResponse;
-import com.sparta.logistics.product.presentation.rest.util.ApiResponse.Success;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "hub-service")
+@FeignClient(name = "hub-company-service")
 public interface HubServiceClient {
 
-    @GetMapping("/api/v1/company/user/{userId}")
-    ResponseEntity<Success<CompanyDetailClientResponse>> getCompanyByUserId(@PathVariable("userId") Long userId);
+    @GetMapping("/api/v1/company/users/{userId}")
+    ResponseEntity<List<CompanyDetailClientResponse>> getCompanyByUserId(@PathVariable("userId") Long userId);
 }
