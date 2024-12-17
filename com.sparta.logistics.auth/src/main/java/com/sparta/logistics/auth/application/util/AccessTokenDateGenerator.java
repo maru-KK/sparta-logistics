f05a3cb3c22ac74b419ec5e9b68f5fb84a55f055
kgrace2366 <kgrace2366@naver.com>
2024-12-17 11:25:02 +0900

    revert: revert unintended file deletions

--- com.sparta.logistics.auth/src/main/java/com/sparta/logistics/auth/application/util/AccessTokenDateGenerator.java
+++ com.sparta.logistics.auth/src/main/java/com/sparta/logistics/auth/application/util/AccessTokenDateGenerator.java
@@ -1,11 +1,9 @@
 package com.sparta.logistics.auth.application.util;
 
-import java.util.Date;
-
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Primary
+import java.util.Date;
+
 @Component
 public class AccessTokenDateGenerator implements DateGenerator {
 
